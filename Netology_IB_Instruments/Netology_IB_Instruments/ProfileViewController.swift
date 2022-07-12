4727624098831e4ced61d68614fe0ee496cce0d9
matakushx <maximkin94@gmail.com>
2022-07-12 14:43:32 +0300

    Add tasks 9-11

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileViewController.swift
@@ -2,22 +2,19 @@
 //  ProfileViewController.swift
 //  Netology_IB_Instruments
 //
-//  Created by Максим on 09.07.2022.
+//  Created by Максим on 12.07.2022.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let profileView = Bundle.main
-            .loadNibNamed("ProfileView", owner: self, options: nil)?
-            .first as! ProfileView
         
-        profileView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-
+        
+        
         // Do any additional setup after loading the view.
     }
     
