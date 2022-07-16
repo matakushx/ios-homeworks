7f07aa4396a0d95512ea2350061642f9700770ec
matakushx <maximkin94@gmail.com>
2022-07-16 14:27:48 +0300

    add task 12

--- Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -8,18 +8,14 @@
 import UIKit
 
 class ProfileView: UIView {
+    
     @IBOutlet weak var userImage: UIImageView!
     @IBOutlet weak var userName: UILabel!
     @IBOutlet weak var userDate: UILabel!
     @IBOutlet weak var userCity: UILabel!
     @IBOutlet weak var userText: UITextView!
     
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
-
 }
+
+    
+
